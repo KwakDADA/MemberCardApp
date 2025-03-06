477f650c6929d326440ca590188e1878a2d93fb5
KwakDADA <down022@gmail.com>
2025-03-06 16:08:06 +0900

    [Refactor] Extract sub-methods from crateLayout of TeamCollectionView

--- MemberCardApp/MemberCardApp/View/TeamCollectionView.swift
+++ MemberCardApp/MemberCardApp/View/TeamCollectionView.swift
@@ -92,59 +92,80 @@ final class TeamCollectionView: UIView {
             guard let self = self,
                   let sections = self.sections else { return nil }
             let section = sections[sectionIndex]
-            
-            let headerItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
-            let headerItem = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerItemSize, elementKind: SupplementaryViewKind.header, alignment: .top)
+
+            let headerItem = createHeaderItem()
             
             switch section {
             case .teamInfo:
-                let itemSize = NSCollectionLayoutSize(
-                    widthDimension: .fractionalWidth(1),
-                    heightDimension: .estimated(100)
-                )
-                let item = NSCollectionLayoutItem(layoutSize: itemSize)
-                
-                let groupSize = NSCollectionLayoutSize(
-                    widthDimension: .fractionalWidth(1),
-                    heightDimension: .estimated(100)
-                )
-                let group = NSCollectionLayoutGroup.horizontal(
-                    layoutSize: groupSize,
-                    subitems: [item]
-                )
-                
-                let section = NSCollectionLayoutSection(group: group)
-                section.contentInsets = .init(top: 0, leading: 0, bottom: 40, trailing: 0)
-                section.boundarySupplementaryItems = [headerItem]
-                
-                return section
+                return self.createTeamInfoSection(supplementaryItems: [headerItem])
                 
             case .memberCard:
-                let itemSize = NSCollectionLayoutSize(
-                    widthDimension: .fractionalWidth(1),
-                    heightDimension: .absolute(400)
-                )
-                let item = NSCollectionLayoutItem(layoutSize: itemSize)
-                
-                let groupSize = NSCollectionLayoutSize(
-                    widthDimension: .fractionalWidth(0.92),
-                    heightDimension: .absolute(400)
-                )
-                let group = NSCollectionLayoutGroup.horizontal(
-                    layoutSize: groupSize,
-                    subitems: [item]
-                )
-                
-                let section = NSCollectionLayoutSection(group: group)
-                section.contentInsets = .init(top: 20, leading: 0, bottom: 40, trailing: 0)
-                section.interGroupSpacing = 8
-                section.orthogonalScrollingBehavior = .groupPagingCentered
-                section.boundarySupplementaryItems = [headerItem]
-                
-                return section
+                return self.createMemberCardSection(supplementaryItems: [headerItem])
             }
         }
         
         return layout
+    }
+    
+    private func createHeaderItem() -> NSCollectionLayoutBoundarySupplementaryItem {
+        let headerItemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .estimated(50)
+        )
+        let headerItem = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerItemSize,
+            elementKind: SupplementaryViewKind.header,
+            alignment: .top
+        )
+        
+        return headerItem
+    }
+    
+    private func createTeamInfoSection(supplementaryItems: [NSCollectionLayoutBoundarySupplementaryItem]) -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .estimated(100)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .estimated(100)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = .init(top: 0, leading: 0, bottom: 40, trailing: 0)
+        section.boundarySupplementaryItems = supplementaryItems
+        
+        return section
+    }
+    
+    private func createMemberCardSection(supplementaryItems: [NSCollectionLayoutBoundarySupplementaryItem]) -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .absolute(400)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.92),
+            heightDimension: .absolute(400)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = .init(top: 20, leading: 0, bottom: 40, trailing: 0)
+        section.interGroupSpacing = 8
+        section.orthogonalScrollingBehavior = .groupPagingCentered
+        section.boundarySupplementaryItems = supplementaryItems
+        
+        return section
     }
 }
