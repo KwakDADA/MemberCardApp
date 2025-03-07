2f84f6a9c1541655f0a4edf2ea0ce8edf4e42656
KwakDADA <down022@gmail.com>
2025-03-07 15:07:15 +0900

    [Cleanup] Remove unnecessary print statements

--- MemberCardApp/MemberCardApp/View/MainViewController.swift
+++ MemberCardApp/MemberCardApp/View/MainViewController.swift
@@ -50,11 +50,8 @@ final class MainViewController: UIViewController {
     // 뷰모델의 멤버 데이터가 갱신되면 컬렉션뷰 스냅샷 업데이트
     private func bindViewModel() {
         viewModel.onMembersUpdated = { [weak self] members in
-            print("onMembersUpdated in MainVC")
             DispatchQueue.main.async {
                 self?.updateSnapshot(with: members)
-                print("스냅샷 업데이트 완료")
-                print(members.map { $0.name })
             }
         }
     }
